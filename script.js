6c0312c3f6d82fe6a9470c74d5b92f76041d32a6
Luma <lumagoesmontes@gmail.com>
2020-05-31 21:52:29 -0300

    adicionado logica de adicao e remocao de quantidade de itens

--- script.js
+++ script.js
@@ -1,133 +1,149 @@
-       function valida(){
-           var nome =form_pedido.nome.value
-           var endereco =form_pedido.endereco.value
-           var numero =form_pedido.numero.value
-           var bairro =form_pedido.bairro.value
-           var cupom =form_pedido.cupom.value
-           var mensagem =form_pedido.mensagem.value
-           var radios = document.getElementsByName("pagamento")
-           var formapagamento = ''
-           
-           if(nome==""){
-           alert("Por favor, informe seu nome completo para o pedido ser realizado.")
-           form_pedido.nome.focus()
-           return false
-                 
-        }
-           
-           
-           if(endereco==""){
-           alert("Por favor, informe seu enderenço.")
-           form_pedido.endereco.focus()
-           return false
-        }
-           
-           if(numero==""){
-           alert("Por favor, informe o número.")
-           form_pedido.numero.focus()
-           return false
-        }
-           
-           if(bairro==""){
-           alert("Por favor, informe o bairro que o pedido será entregue")
-           form_pedido.bairro.focus()
-           return false 
-        }
+$(function(){
+   $('#i-sobre').click(function(i){
+      $("#lanches").addClass("hidden")
+     $("#bebidas").addClass("hidden")
+     $("#pedido").addClass("hidden")
+     $("#adicionais").addClass("hidden")
 
-        if(radios[0].checked){
-           formapagamento = 'Dinheiro'
-           return true
+     $('#sobre').removeClass('hidden');
+     $(this).addClass('active');
+     
+   });
+}); 
 
-           
-        } else if (radios[1].checked){
-           formapagamento = 'Cartão de Crédito'
-           return true
+$(function(){
+   $('#i-lan').click(function(i){
+      $("#sobre").addClass("hidden")
+      $("#bebidas").addClass("hidden")
+      $("#pedido").addClass("hidden")
+      $("#adicionais").addClass("hidden")
 
+       $('#lanches').removeClass('hidden');
+       $(this).addClass('active');
 
-        } else {
-           formapagamento =" Cartão de Débito"
-        } 
+   });
+});     
 
-        if (radios[0].checked == false && radios[1].checked == false && radios[2].checked == false){
-           alert("Escolha uma forma de pagamento, por favorzi")
-         radios.focus()
-         return false
-        }
-    } 
+$(function(){
+$('#i-bebidas').click(function(i){
+   $("#lanches").addClass("hidden")
+   $("#sobre").addClass("hidden")
+   $("#pedido").addClass("hidden")
+   $("#adicionais").addClass("hidden")
 
-//     document.getElementById("btnSubmit").onclick = function() {
-      
-//       for (var i = 0; i < radios.length; i++) {
-//           if (radios[i].checked) {
-//               console.log("Escolheu: " + radios[i].value);
-//           }
-//       }
-//   };
+    $('#bebidas').removeClass('hidden');
+    $(this).addClass('active');
 
-   //  document.getElementsByClassName("i-lanches").click(function(){
-   //     document.getElementById("lanches
-   //  })
+});
+}); 
+
+$(function(){
+$('#i-adicionais').click(function(i){
+   $("#lanches").addClass("hidden")
+   $("#sobre").addClass("hidden")
+   $("#pedido").addClass("hidden")
+   $("#bebidas").addClass("hidden")
+    $('#adicionais').removeClass('hidden');
+    $(this).addClass('active');
+
+});
+}); 
+
+$(function(){
+$('#i-pedidos').click(function(i){
+   $("#lanches").addClass("hidden")
+   $("#sobre").addClass("hidden")
+   $("#adicionais").addClass("hidden")
+   $("#bebidas").addClass("hidden")
+
+    $('#pedido').removeClass('hidden');
+    $(this).addClass('active');
+
+});
+});    
        
-   $(function(){
-      $('#i-sobre').click(function(i){
-         $("#lanches").addClass("hidden")
-        $("#bebidas").addClass("hidden")
-        $("#pedido").addClass("hidden")
-        $("#adicionais").addClass("hidden")
+      function valida(){
+         var nome =form_pedido.nome.value
+         var endereco =form_pedido.endereco.value
+         var numero =form_pedido.numero.value
+         var bairro =form_pedido.bairro.value
+         var cupom =form_pedido.cupom.value
+         var mensagem =form_pedido.mensagem.value
+         var radios = document.getElementsByName("pagamento")
+         var formapagamento = ''
+         
+         if(nome==""){
+         alert("Por favor, informe seu nome completo para o pedido ser realizado.")
+         form_pedido.nome.focus()
+         return false
+               
+      }
+         
+         
+         if(endereco==""){
+         alert("Por favor, informe seu enderenço.")
+         form_pedido.endereco.focus()
+         return false
+      }
+         
+         if(numero==""){
+         alert("Por favor, informe o número.")
+         form_pedido.numero.focus()
+         return false
+      }
+         
+         if(bairro==""){
+         alert("Por favor, informe o bairro que o pedido será entregue")
+         form_pedido.bairro.focus()
+         return false 
+      }
 
-        $('#sobre').removeClass('hidden');
-        $(this).addClass('active');
-        
-      });
-   }); 
+      if(radios[0].checked){
+         formapagamento = 'Dinheiro'
+         return true
+
+         
+      } else if (radios[1].checked){
+         formapagamento = 'Cartão de Crédito'
+         return true
+
+
+      } else {
+         formapagamento =" Cartão de Débito"
+      } 
+      if (radios[0].checked == false && radios[1].checked == false && radios[2].checked == false){
+         alert("Escolha uma forma de pagamento, por favorzi")
+      radios.focus()
+      return false
+      }
+   } 
+
+
+
+   var itens = document.getElementsByClassName("lanches_info")[0]
+   var itensQuantidade = itens.getElementsByClassName("lanches_quant")[0]
+   var item = itensQuantidade.getElementsByClassName('item_quant').innerHTML
    
-   $(function(){
-      $('#i-lan').click(function(i){
-         $("#sobre").addClass("hidden")
-         $("#bebidas").addClass("hidden")
-         $("#pedido").addClass("hidden")
-         $("#adicionais").addClass("hidden")
+   btnAdicionar.addEventListener('click', () => {
+    var itens = document.getElementsByClassName("lanches_info")[0]
+    var itensQuantidade = itens.getElementsByClassName("lanches_quant")[0]
+    var item = itensQuantidade.getElementsByClassName('item_quant')[0]
+    item.innerHTML = Number(item.innerHTML) + 1;
+})
 
-          $('#lanches').removeClass('hidden');
-          $(this).addClass('active');
- 
-      });
-  });     
 
-$(function(){
-   $('#i-bebidas').click(function(i){
-      $("#lanches").addClass("hidden")
-      $("#sobre").addClass("hidden")
-      $("#pedido").addClass("hidden")
-      $("#adicionais").addClass("hidden")
+function retirarItem(){
+   var item = itensQuantidade.getElementsByClassName('item_quant')[0].innerHTML
+   item--;
+   itensQuantidade.getElementsByClassName('item_quant')[0].innerHTML = item;
+}
 
-       $('#bebidas').removeClass('hidden');
-       $(this).addClass('active');
+   var btnRemove = itens.getElementsByClassName("btnB")[0]
+   btnRemove.addEventListener('click', retirarItem)
 
-   });
-}); 
 
-$(function(){
-   $('#i-adicionais').click(function(i){
-      $("#lanches").addClass("hidden")
-      $("#sobre").addClass("hidden")
-      $("#pedido").addClass("hidden")
-      $("#bebidas").addClass("hidden")
-       $('#adicionais').removeClass('hidden');
-       $(this).addClass('active');
 
-   });
-}); 
 
-$(function(){
-   $('#i-pedidos').click(function(i){
-      $("#lanches").addClass("hidden")
-      $("#sobre").addClass("hidden")
-      $("#adicionais").addClass("hidden")
-      $("#bebidas").addClass("hidden")
 
-       $('#pedido').removeClass('hidden');
-       $(this).addClass('active');
+       
 
-   });
-}); 
