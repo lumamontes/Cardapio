34df3ebfbfdc08bc7ef8a633f041464df0e7d32a
Alessandro Rodrigues <65730229+forge1010@users.noreply.github.com>
2020-06-02 23:18:29 -0300

    validações feitas e funcionais

--- script.js
+++ script.js
@@ -63,61 +63,99 @@ $('#i-pedidos').click(function(i){
 });
 }); 
        
-      function valida(){
-         var nome =form_pedido.nome.value
-         var endereco =form_pedido.endereco.value
-         var numero =form_pedido.numero.value
-         var bairro =form_pedido.bairro.value
-         var cupom =form_pedido.cupom.value
-         var mensagem =form_pedido.mensagem.value
-         var radios = document.getElementsByName("pagamento")
-         var formapagamento = ''
-         console.log(valida)
-         if(nome==""){
-         alert("Por favor, informe seu nome completo para o pedido ser realizado.")
-         form_pedido.nome.focus()
-         return false
-               
+     function nome(){
+        var nome =form_pedido.nome.value
+        if(nome==""){
+        alert("Por favor, informe seu nome completo.")
+        return false
       }
-         
-         
-         if(endereco==""){
-         alert("Por favor, informe seu enderenço.")
-         form_pedido.endereco.focus()
+      var regex=/[0-9]/
+      if(regex.test(nome)==true){
+         alert("Seu nome não pode ter números.")
          return false
+      
       }
-         
-         if(numero==""){
-         alert("Por favor, informe o número.")
-         form_pedido.numero.focus()
-         return false
+      var regexa=/[!-@]/
+      if(regexa.test(nome)==true){
+         alert("nao pode")
       }
+      if(nome.length>=30){
+         alert("Por favor, digite até 30 caracteres!")
          
-         if(bairro==""){
-         alert("Por favor, informe o bairro que o pedido será entregue")
-         form_pedido.bairro.focus()
-         return false 
+      }
+      if(nome.length<=2){
+         alert("Digite seu nome.")
       }
 
+     }
+     function endereco(){
+        var endereco =form_pedido.endereco.value
+        if(endereco==""){
+        alert("Por favor, informe seu endereço.")
+      }
+       var regex=/[0-9]/
+       if(regex.test(endereco)==true){
+          alert("Seu endereço não pode ter numeros.")
+       }
+     }
+
+     function numero(){
+        var numero =form_pedido.numero.value
+        if(numero==""){
+           alert("Informe o número da sua casa ou apartamento.")
+        }
+        var regex=/[A-za-z]/
+        if(regex.test(numero)==true){
+           alert("Coloque apenas números.")
+        }
+        if(numero.length>=5){
+           alert("digite um numero válido")
+        }
+     }
+     function bairro(){
+        var bairro =form_pedido.bairro.value
+        if(bairro==""){
+           alert("Por favor, informe seu bairro.")
+        }
+        var regex=/[0-9]/
+        if(regex.test(bairro)==true){
+           alert("Coloque somente letras ao informar o seu bairro")
+        }
+     }
+     function radio(){
+      var radios= document.getElementsByName("pagamento")
+      var formapagamento=""
+     
       if(radios[0].checked){
          formapagamento = 'Dinheiro'
          return true
-
+ 
          
       } else if (radios[1].checked){
          formapagamento = 'Cartão de Crédito'
          return true
-
-
+ 
+ 
       } else {
          formapagamento =" Cartão de Débito"
       } 
-      if (radios[0].checked == false && radios[1].checked == false && radios[2].checked == false){
-         alert("Escolha uma forma de pagamento, por favorzi")
-      radios.focus()
-      return false
+      if(radios[0].checked== false && radios[1].checked==false&&radios[2].checked==false){
+         alert("Escolha uma forma de pagamento.")
+         radios.focus()
+         return false
       }
-   } 
+   }
+
+     
+
+     function valida(){
+        nome()
+        endereco()
+        numero()
+        bairro()
+        radio()
+     };
+
 
 
 
@@ -144,15 +182,7 @@ function retirarItem(){
    btnRemove.addEventListener('click', retirarItem)
    console.log("lanches_info")
 
-   function nome(){
-      var nome= form_pedido.nome.value
-      if(nome==""){
-          alert("Por favor, informe seu nome completo para o pedido ser realizado")
-          return false
-      }
-      var regex=/[]/
-  }
-   
+      
 
 
 
