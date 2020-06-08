2df4901e4a614599847dae7afc088fe562e1e574
Luma <lumagoesmontes@gmail.com>
2020-06-08 12:49:11 -0300

    feito lógica pra retornar no link do wpp a forma de pagamento, os itens selecionados e o valor deles, e também o valor total \o/

--- js/wpp.js
+++ js/wpp.js
@@ -1,28 +1,42 @@
-    // Enviar no wpp
-    // Atualmente, só as informações adicionadas form
-    // Pensar em lógica pra puxar infos dos itens também 
-
-    console.log(radio)
- 
-    
-    $("#enviar").click(function(){
+$(Document).ready(function(){
+      $("#enviar").click(function(){
        var nome=$("#nome").val()
        var endereco=$("#endereco").val()
        var numero=$("#numero").val()
        var bairro=$("#bairro").val()
-       var pagamento=$("#dinheiro").val()
-        var texto=`${pedido},${nome}, ${pagamento}, ${bairro}, ${endereco}, ${numero}`
-       if(nome=="")
+       const formasDePagamento= document.querySelectorAll("form input[type=radio]")
+       var formaSelecionada = ""
+       var pedido = ""
+       if(formasDePagamento[0].checked){
+           formaSelecionada = "Dinheiro"
+         } else if (formasDePagamento[1].checked) {
+             formaSelecionada =" Cartão de Crédito"
+         } else {
+             formaSelecionada = "cartão de Débito"
 
-    {alert("digite as informações corretas né")
-  }else{
-      var texto=`${nome}, ${pagamento}, ${bairro}, ${endereco}, ${numero} Vim através do sistema`
-      var site="https://api.whatsapp.com/send?phone=5596991410023&text="+texto.replace(" ","%20")
+         }
+         const pedidosDisponiveis = document.querySelectorAll("input[type=checkbox]")       
+         var pedidoSelecionado = ""
+         var soma = 0; 
+         for ( pedido of pedidosDisponiveis) {
+                 var pedidoSelecionados = pedido.checked
+                 
+                 if(pedido.checked == true) {
+                     pedidoSelecionado +=` ${pedido.id}, ${pedido.value} Reais` 
+                     var pedidosValores  =  parseInt(pedido.value)
+                     soma += pedidosValores   
+                 }
+         }
+
+       var texto=`${nome},${endereco}, ${bairro}, ${numero}, ${formaSelecionada}, ${pedidoSelecionado}, ${soma}`
+       
+      var site="https://api.whatsapp.com/send?phone=5596991410023"+texto.replace(" ","%20")
       window.location.href=site;
+        console.log(site)
 
-  }
-     })
-
+    })
+})
+   
 
 
   
