f4c91a03ca376c9f3b30baae1f9bce1650370fc3
Luma <lumagoesmontes@gmail.com>
2020-06-15 16:11:38 -0300

    ajuste adicionais + comentarios

--- js/script.js
+++ js/script.js
@@ -1,120 +1,96 @@
-        //Validações
+   //      //Validações
         
-        function nome(){
-        var nome =form_pedido.nome.value
-        if(nome==""){
-        alert("Por favor, informe seu nome completo.")
-        return false
-      }
-      var regex=/[0-9]/
-      if(regex.test(nome)==true){
-         alert("Seu nome não pode ter números.")
-         return false
+   //      function nome(){
+   //      var nome =form_pedido.nome.value
+   //      if(nome==""){
+   //      alert("Por favor, informe seu nome completo.")
+   //      return false
+   //    }
+   //    var regex=/[0-9]/
+   //    if(regex.test(nome)==true){
+   //       alert("Seu nome não pode ter números.")
+   //       return false
       
-      }
-      var regexa=/[!-@]/
-      if(regexa.test(nome)==true){
-         alert("nao pode")
-      }
-      if(nome.length>=30){
-         alert("Por favor, digite até 30 caracteres!")
+   //    }
+   //    var regex=/[!-@]/
+   //    if(regex.test(nome)==true){
+   //       alert("nao pode")
+   //    }
+   //    if(nome.length>=30){
+   //       alert("Por favor, digite até 30 caracteres!")
          
-      }
-      if(nome.length<=2){
-         alert("Digite seu nome.")
-      }
+   //    }
+   //    if(nome.length<=2){
+   //       alert("Digite seu nome.")
+   //    }
 
-     }
-     function endereco(){
-        var endereco =form_pedido.endereco.value
-        if(endereco==""){
-        alert("Por favor, informe seu endereço.")
-      }
-       var regex=/[0-9]/
-       if(regex.test(endereco)==true){
-          alert("Seu endereço não pode ter numeros.")
-       }
-     }
+   //   }
+   //   function endereco(){
+   //      var endereco =form_pedido.endereco.value
+   //      if(endereco==""){
+   //      alert("Por favor, informe seu endereço.")
+   //    }
+   //     var regex=/[0-9]/
+   //     if(regex.test(endereco)==true){
+   //        alert("Seu endereço não pode ter numeros.")
+   //     }
+   //   }
 
-     function numero(){
-        var numero =form_pedido.numero.value
-        if(numero==""){
-           alert("Informe o número da sua casa ou apartamento.")
-        }
-        var regex=/[A-za-z]/
-        if(regex.test(numero)==true){
-           alert("Coloque apenas números.")
-        }
-        if(numero.length>=5){
-           alert("digite um numero válido")
-        }
-     }
-     function bairro(){
-        var bairro =form_pedido.bairro.value
-        if(bairro==""){
-           alert("Por favor, informe seu bairro.")
-        }
-        var regex=/[0-9]/
-        if(regex.test(bairro)==true){
-           alert("Coloque somente letras ao informar o seu bairro")
-        }
-     }
-     function radio(){
-      var radios= document.getElementsByName("pagamento")
-      var formapagamento=""
+   //   function numero(){
+   //      var numero =form_pedido.numero.value
+   //      if(numero==""){
+   //         alert("Informe o número da sua casa ou apartamento.")
+   //      }
+   //      var regex=/[A-za-z]/
+   //      if(regex.test(numero)==true){
+   //         alert("Coloque apenas números.")
+   //      }
+   //      if(numero.length>=5){
+   //         alert("digite um numero válido")
+   //      }
+   //   }
+   //   function bairro(){
+   //      var bairro =form_pedido.bairro.value
+   //      if(bairro==""){
+   //         alert("Por favor, informe seu bairro.")
+   //      }
+   //      var regex=/[0-9]/
+   //      if(regex.test(bairro)==true){
+   //         alert("Coloque somente letras ao informar o seu bairro")
+   //      }
+   //   }
+   //   function radio(){
+   //    var radios= document.getElementsByName("pagamento")
+   //    var formapagamento=""
      
-      if(radios[0].checked){
-         formapagamento = 'Dinheiro'
-         return true
+   //    if(radios[0].checked){
+   //       formapagamento = 'Dinheiro'
+   //       return true
  
          
-      } else if (radios[1].checked){
-         formapagamento = 'Cartão de Crédito'
-         return true
+   //    } else if (radios[1].checked){
+   //       formapagamento = 'Cartão de Crédito'
+   //       return true
  
  
-      } else {
-         formapagamento =" Cartão de Débito"
-      } 
-      if(radios[0].checked== false && radios[1].checked==false&&radios[2].checked==false){
-         alert("Escolha uma forma de pagamento.")
-         radios.focus()
-         return false
-      }
-   }     
+   //    } else {
+   //       formapagamento =" Cartão de Débito"
+   //    } 
+   //    if(radios[0].checked== false && radios[1].checked==false&&radios[2].checked==false){
+   //       alert("Escolha uma forma de pagamento.")
+   //       radios.focus()
+   //       return false
+   //    }
+   // }     
 
-   function valida(){
-        nome()
-        endereco()
-        numero()
-        bairro()
-        radio()
-     };
+   // function valida(){
+   //      nome()
+   //      endereco()
+   //      numero()
+   //      bairro()
+   //      radio()
+   //   };
 
-
-     //Adicionar quantidade de itens
-
-//      var itens = document.getElementsByClassName("item_info")[1]
-//      var itensQuantidade = itens.getElementsByClassName("item_quant")[0]
-//      var item = itensQuantidade.getElementsByClassName('item_quant').innerHTML
-//      var btnAdicionar = itens.getElementsByClassName("btnA")[0]
-     
-//      btnAdicionar.addEventListener('click', () => {
-//       var itens = document.getElementsByClassName("lanches_info")[0]
-//       var itensQuantidade = itens.getElementsByClassName("lanches_quant")[0]
-//       var item = itensQuantidade.getElementsByClassName('item_quant')[0]
-//       item.innerHTML = Number(item.innerHTML) + 1;
-//   })
-    
-//   function retirarItem(){
-//      var item = itensQuantidade.getElementsByClassName('item_quant')[0].innerHTML
-//      item--;
-//      itensQuantidade.getElementsByClassName('item_quant')[0].innerHTML = item;
-//   }
-  
-//      var btnRemove = itens.getElementsByClassName("btnB")[0]
-//      btnRemove.addEventListener('click', retirarItem)
-//      console.log("lanches_info")
 
 
     
--- js/wpp.js
+++ js/wpp.js
@@ -1,4 +1,4 @@
-//BOtão de enviar no WhatsApp
+//BOtão de enviar as informações no WhatsApp
 
 $(document).ready(function(){
       $("#enviar").click(function(){
@@ -7,6 +7,8 @@ $(document).ready(function(){
        var numero=$("#numero").val()
        var bairro=$("#bairro").val()
        var observacao = $("textarea").val()
+       
+       // FORMA de pagamento selecionada
        const formasDePagamento= document.querySelectorAll("form input[type=radio]")
        var formaSelecionada = ""
        if(formasDePagamento[0].checked){
@@ -17,36 +19,49 @@ $(document).ready(function(){
             formaSelecionada = "cartão de Débito"
             
         }
+
+        // Valor do Pedido
+        var soma = 0; 
+       
+        // Pedido Selecionado
         
         var pedido = "" 
         const pedidosDisponiveis = document.querySelectorAll("input[type=checkbox]")
         var pedidoSelecionado = ""
-        var soma = 0; 
+    
         for ( pedido of pedidosDisponiveis) {
             var pedidoSelecionados = pedido.checked
             
             if(pedido.checked == true) {
-                pedidoSelecionado +=` ${pedido.id}, ${pedido.value} Reais` 
+                pedidoSelecionado +=` ${pedido.id} -> ${pedido.value} Reais; ` 
                 var pedidosValores  =  parseInt(pedido.value)
                 soma += pedidosValores   
             }
         }
+
+        // Adicional Selecionado
         
         const AdicionaisDisponiveis = document.querySelectorAll(".input-add")
         var adicionalSelecionado = ""
         for (adicional of AdicionaisDisponiveis){
             if(adicional.checked == true){
-                adicionalSelecionado += `Adicional - ${adicional.id}, ${adicional.value} Reais`
+                adicionalSelecionado += `${adicional.alt}: ${adicional.id} -> ${adicional.value} Reais;`
                 var adicionaisValores = parseInt(adicional.value)
                 soma+= adicionaisValores
             }
             
         }       
-        
-        var texto=`${nome} ,${endereco} , ${bairro}, ${numero}, ${observacao}, ${formaSelecionada} , ${pedidoSelecionado} , ${adicionalSelecionado}, Valor Total = ${soma}`
+        var valorTotal = soma + TaxaDeEntrega
+        var texto=`${nome};${endereco} ; ${bairro}; ${numero}; ${observacao}; 
+        // Forma de pagamento: ${formaSelecionada}, Pedido: ${pedidoSelecionado}, Adicionais: ${adicionalSelecionado},
+         Valor Pedido = ${soma},00 
+         ;Taxa de entrega: 
+         ;Valor Total:` 
         
         var site="https://api.whatsapp.com/send?phone=5596991410023"+texto.replace(" ","%20")
         window.location.href= site;
+        console.log(site)
+
         
     })
 })
